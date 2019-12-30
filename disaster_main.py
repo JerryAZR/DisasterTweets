b6a58bfa504122a968e1754a4bc24174dfc1d83c
Zerui An <anzerui@126.com>
2019-12-30 18:20:42 -0500

    first commit

---
+++ disaster_main.py
@@ -0,0 +1,36 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in
+
+import numpy as napi # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split
+# Input data files are available in the "../input/" directory.
+
+sample_submission = pd.read_csv("../input/nlp-getting-started/sample_submission.csv")
+test = pd.read_csv("../input/nlp-getting-started/test.csv")
+train = pd.read_csv("../input/nlp-getting-started/train.csv")
+
+X = train.iloc[:, 1:-1]
+y = train["target"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X.head()
+
+# keyword dictionary
+keyword_arr = X_train.iloc[:,1].values
+keyword_arr = keyword_arr[~pd.isnull(keyword_arr)]
+print(keyword_arr.size)
+keyword_arr = napi.unique(keyword_arr)
+print(keyword_arr.size)
+KEYWORD_ARRAY_SIZE = keyword_arr.size
+
+# TODO: Coonvert keywords to feature vectors
+
+# location dictionary
+location_arr = X_train.iloc[:,2].values
+location_arr = location_arr[~pd.isnull(location_arr)]
+print(location_arr.size)
+location_arr = napi.unique(location_arr)
+print(location_arr.size)
+LOCATION_ARRAY_SIZE = location_arr.size
+print(location_arr)
